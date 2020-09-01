7706a5453f5c4c0df18d740bb4ef84b3e3b4c29b
Akshat Gupta <akshatvg23@gmail.com>
2020-09-01 23:35:12 +0530

    basic tools added

---
+++ index.js
@@ -0,0 +1,146 @@
+// Color Variable
+var color;
+
+// Clear the canvas
+function clearCanvas() {
+    canvas.width = canvas.width;
+    console.log("Cleared Canvas.");
+}
+
+// Color picker stroke color
+function changeColor() {
+    color = "#000000";
+    ctx.strokeStyle = color;
+    document.getElementById("colorPicker").value = "#000000";
+    ctx.lineWidth = 2;
+    document.getElementById("colorPicker").click();
+    document.getElementById("colorPicker").onchange = function () {
+        color = this.value;
+        console.log("Color changed to:" + color);
+        ctx.strokeStyle = color;
+        ctx.lineWidth = 2;
+    }
+}
+
+// Eraser
+function startErasing() {
+    color = this.value;
+    console.log("Erasing.");
+    ctx.strokeStyle = "#FFFFFF";
+    ctx.lineWidth = 50;
+}
+
+// Set up the canvas
+var canvas = document.getElementById("whiteboard-canvas");
+var dpr = window.devicePixelRatio || 1;
+var ctx = canvas.getContext("2d");
+ctx.canvas.width = window.innerWidth * dpr;
+ctx.canvas.height = window.innerHeight * dpr;
+ctx.strokeStyle = color;
+ctx.lineWith = 2;
+
+// Set up mouse events for drawing
+var drawing = false;
+var mousePos = { x: 0, y: 0 };
+var lastPos = mousePos;
+canvas.addEventListener("mousedown", function (e) {
+    drawing = true;
+    lastPos = getMousePos(canvas, e);
+}, false);
+canvas.addEventListener("mouseup", function (e) {
+    drawing = false;
+}, false);
+canvas.addEventListener("mousemove", function (e) {
+    mousePos = getMousePos(canvas, e);
+}, false);
+
+// Get the position of the mouse relative to the canvas
+function getMousePos(canvasDom, mouseEvent) {
+    var rect = canvasDom.getBoundingClientRect();
+    return {
+        x: mouseEvent.clientX - rect.left,
+        y: mouseEvent.clientY - rect.top
+    };
+}
+
+// Get a regular interval for drawing to the screen
+window.requestAnimFrame = (function (callback) {
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimaitonFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+// Draw to the canvas
+function renderCanvas() {
+    if (drawing) {
+        ctx.beginPath();
+        ctx.moveTo(lastPos.x, lastPos.y);
+        ctx.lineTo(mousePos.x, mousePos.y);
+        ctx.stroke();
+        lastPos = mousePos;
+        console.log(lastPos);
+        ctx.closePath();
+    }
+}
+
+// Allow for animation
+(function drawLoop() {
+    requestAnimFrame(drawLoop);
+    renderCanvas();
+})();
+
+// Set up touch events for mobile, etc
+canvas.addEventListener("touchstart", function (e) {
+    mousePos = getTouchPos(canvas, e);
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousedown", {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchend", function (e) {
+    var mouseEvent = new MouseEvent("mouseup", {});
+    canvas.dispatchEvent(mouseEvent);
+}, false);
+canvas.addEventListener("touchmove", function (e) {
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousemove", {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
+}, false);
+
+// Get the position of a touch relative to the canvas
+function getTouchPos(canvasDom, touchEvent) {
+    var rect = canvasDom.getBoundingClientRect();
+    return {
+        x: touchEvent.touches[0].clientX - rect.left,
+        y: touchEvent.touches[0].clientY - rect.top
+    };
+}
+
+// Prevent scrolling when touching the canvas
+document.body.addEventListener("touchstart", function (e) {
+    if (e.target == canvas) {
+        e.preventDefault();
+    }
+}, false);
+document.body.addEventListener("touchend", function (e) {
+    if (e.target == canvas) {
+        e.preventDefault();
+    }
+}, false);
+document.body.addEventListener("touchmove", function (e) {
+    if (e.target == canvas) {
+        e.preventDefault();
+    }
+}, false);
+
+var dataUrl = canvas.toDataURL();
